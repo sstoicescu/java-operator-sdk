fd32b67ed5beb2ae9ee15d9a85a1d4e31ccbf815
Sorin Stoicescu <sorin.stoicescu.2012@gmail.com>
2020-08-04 13:40:21 +0200

    Hackathon code

--- samples/spring-boot-plain/src/main/java/com/github/containersolutions/operator/sample/Config.java
+++ samples/spring-boot-plain/src/main/java/com/github/containersolutions/operator/sample/Config.java
@@ -3,12 +3,17 @@ package com.github.containersolutions.operator.sample;
 import com.github.containersolutions.operator.Operator;
 import com.github.containersolutions.operator.api.ResourceController;
 import com.github.containersolutions.operator.processing.retry.GenericRetry;
+
+import io.fabric8.kubernetes.client.CustomResource;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.dsl.internal.CustomResourceOperationsImpl;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
+import java.util.Map;
 
 @Configuration
 public class Config {
@@ -24,12 +29,22 @@ public class Config {
     }
 
     //  Register all controller beans
-    @Bean
+    @SuppressWarnings("unchecked")
+	@Bean
     public Operator operator(KubernetesClient client, List<ResourceController> controllers) {
         Operator operator = new Operator(client);
-        controllers.forEach(c -> operator.registerControllerForAllNamespaces(c,
-                GenericRetry.defaultLimitedExponentialRetry()));
+        controllers.forEach(c -> {operator.registerControllerForAllNamespaces(c,
+                GenericRetry.defaultLimitedExponentialRetry());
+        if (c instanceof CustomServiceController) {
+        	((CustomServiceController)c).setCustomResourceClient(operator.getCustomResourceClients());
+        }
+        });
         return operator;
     }
+    
+//    @Bean
+//    public Map<Class<? extends CustomResource>, CustomResourceOperationsImpl> getCustomResourceClient(Operator operator) {
+//    	return operator.getCustomResourceClients();
+//    }
 
 }
